1f24268b0e5a53dbb9f3339c767a05d7847e782d
ilhamizzh's <achmadhefni@Ilhams-MacBook-Air.local>
2025-12-10 23:08:06 +0700

    [feat] nav menu button on Landing Page

--- irms-web/src/app/page.tsx
+++ irms-web/src/app/page.tsx
@@ -1,10 +1,12 @@
 import HeroSection from "@/components/HeroSection";
 import DateSection from "@/components/DateSection";
 import NewsRunningText from "@/components/NewsRunningText";
+import OverlayNav from "@/components/OverlayNav";
 
 export default function Home() {
   return (
     <main>
+      <OverlayNav />
       <HeroSection />
       <DateSection />
       <NewsRunningText />
